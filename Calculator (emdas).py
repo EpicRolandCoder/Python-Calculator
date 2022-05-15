50b5911fa79a9c9dbd0af5a5a16559e6923d8fb9
EpicRolandCoder <99000345+EpicRolandCoder@users.noreply.github.com>
2022-05-16 09:07:15 +1000

    Add files via upload
    Python Calculator: Update 0.3
    The Calculator now accepts exponents via a "^" symbol. Consider it now an "emdas" calculator.

---
+++ Calculator (emdas).py
@@ -0,0 +1,61 @@
+'''numsent = ["2", "+", "8", "/", "2", "*", "4"]
+
+   numsent[numsent.index("/")-1] = int(numsent[numsent.index("/")-1]) / int(numsent[numsent.index("/")+1])
+   numsent.pop(numsent.index("/")+1)
+   numsent.pop(numsent.index("/"))'''
+
+def addsub(numsent):
+    while ("+" in numsent) and ("-" in numsent):
+        if numsent.index("-") < numsent.index("+"):
+            numsent[numsent.index("-")-1] = float(numsent[numsent.index("-")-1]) - float(numsent[numsent.index("-")+1])
+            numsent.pop(numsent.index("-")+1)
+            numsent.pop(numsent.index("-"))
+        elif numsent.index("+") < numsent.index("-"):
+            numsent[numsent.index("+")-1] = float(numsent[numsent.index("+")-1]) + float(numsent[numsent.index("+")+1])
+            numsent.pop(numsent.index("+")+1)
+            numsent.pop(numsent.index("+"))
+    while "+" in numsent:
+        numsent[numsent.index("+")-1] = float(numsent[numsent.index("+")-1]) + float(numsent[numsent.index("+")+1])
+        numsent.pop(numsent.index("+")+1)
+        numsent.pop(numsent.index("+"))
+    while "-" in numsent:
+        numsent[numsent.index("-")-1] = float(numsent[numsent.index("-")-1]) - float(numsent[numsent.index("-")+1])
+        numsent.pop(numsent.index("-")+1)
+        numsent.pop(numsent.index("-"))
+
+
+def multdiv(numsent):
+    while ("/" in numsent) and ("*" in numsent):
+        if numsent.index("/") < numsent.index("*"):
+            numsent[numsent.index("/")-1] = float(numsent[numsent.index("/")-1]) / float(numsent[numsent.index("/")+1])
+            numsent.pop(numsent.index("/")+1)
+            numsent.pop(numsent.index("/"))
+        elif numsent.index("*") < numsent.index("/"):
+            numsent[numsent.index("*")-1] = float(numsent[numsent.index("*")-1]) * float(numsent[numsent.index("*")+1])
+            numsent.pop(numsent.index("*")+1)
+            numsent.pop(numsent.index("*"))
+    while "*" in numsent:
+        numsent[numsent.index("*")-1] = float(numsent[numsent.index("*")-1]) * float(numsent[numsent.index("*")+1])
+        numsent.pop(numsent.index("*")+1)
+        numsent.pop(numsent.index("*"))
+    while "/" in numsent:
+        numsent[numsent.index("/")-1] = float(numsent[numsent.index("/")-1]) / float(numsent[numsent.index("/")+1])
+        numsent.pop(numsent.index("/")+1)
+        numsent.pop(numsent.index("/"))
+
+def exponent(numsent):
+    while "^" in numsent:
+        numsent[numsent.index("^")-1] = float(numsent[numsent.index("^")-1]) ** float(numsent[numsent.index("^")+1])
+        numsent.pop(numsent.index("^")+1)
+        numsent.pop(numsent.index("^"))
+    
+
+def simplify(numsent):
+    numsent = numsent.split()
+    exponent(numsent)
+    multdiv(numsent)
+    addsub(numsent)
+    print(numsent)
+
+userinput = input("Enter an equation. Keep spaces between numbers and operators. The program relies on an equation being entered correctly: ")
+simplify(userinput)
